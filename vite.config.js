9944017252139b2c9004ae24a81b69a3e63090d0
huchenxi <chenxihu@gmail.com>
2024-07-13 17:43:25 +0800

    完善scss,tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +10,11 @@ export default defineConfig({
   resolve:{
     alias:{
       '@': path.resolve(__dirname, 'src'),
+    }
+  },
+  css:{
+    postcss:{
+      plugins:[tailwindcss,autoprefixer]
     }
   }
 })
